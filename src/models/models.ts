7750e6306b76f759231e8c9cac4b56e72d1c5892
JohanTran <johan.tran0210@gmail.com>
2024-09-27 21:35:31 +0200

    error handling for mongoose errors fixed #2

--- src/models/models.ts
+++ src/models/models.ts
@@ -3,9 +3,16 @@ import { ITask } from "../resources/Task/types.ts";
 import { IUser } from "../resources/User/types.ts";
 
 const taskSchema = new Schema<ITask, Model<ITask>>({
-    title: { type: String },
-    description: { type: String },
-    status: { type: String, default: "to-do" },
+    title: { type: String, required: [true, "Title is required"] },
+    description: { type: String, required: [true, "Description is required"] },
+    status: {
+        type: String,
+        default: "to-do",
+        enum: {
+            values: ["to-do", "in-progress", "blocked", "done"],
+            message: "{VALUE} is not supported for status"
+        },
+    },
     assignedTo: { type: [String] },
     createdAt: { type: Date, default: Date.now },
     finishedBy: { type: String },
@@ -14,8 +21,8 @@ const taskSchema = new Schema<ITask, Model<ITask>>({
 
 const userSchema = new Schema<IUser, Model<IUser>>({
     name: { type: String },
-    email: { type: String },
-    password: { type: String },
+    email: { type: String, required: [true, "Email is required"] },
+    password: { type: String, required: [true, "Password is required"] },
     role: { type: String, default: "user" },
 })
 
--- src/resources/Task/controller.ts
+++ src/resources/Task/controller.ts
@@ -1,6 +1,9 @@
 import { Task } from "../../models/models.ts";
 import { Request, Response } from "express";
 import { ITask } from "./types.ts";
+import { asyncErrorHandler } from "../Error/asyncErrorHandler.ts";
+import { CustomError } from "../Error/types.ts";
+
 // Möjlighet att skapa, läsa, uppdatera och ta bort en Task
 /* 
 Create Task
@@ -15,80 +18,68 @@ DELETE Delete one task api/tasks/:taskId
 POST Create one task api/tasks
 */
 
-async function getTasks(req: Request, res: Response) {
-    try {
-        const tasks: ITask[] = await Task.find();
-        return res.status(200).json(tasks);
-    } catch (e) {
-        console.error("Error details:", e);
-        res.status(500).json({ e: "Database query failed!" });
-    }
-}
+const getTasks = asyncErrorHandler(async (req: Request, res: Response) => {
+    const tasks: ITask[] = await Task.find();
 
-async function getTask(req: Request, res: Response) {
+    res.status(200).json({
+        status: "success",
+        data: tasks
+    });
+})
+
+const getTask = asyncErrorHandler(async (req: Request, res: Response) => {
     const { taskId } = req.params;
 
-    try {
-        const task = await Task.findById(taskId);
+    const task = await Task.findById(taskId);
 
-        if (!task) res.status(404).send("Task not found");
-        else return res.status(200).json(task);
+    if (!task) throw new CustomError("Task not found", 404);
 
-    } catch (e) {
-        console.error("Error details:", e);
-        res.status(500).json("Database query failed!");
-    }
-}
+    res.status(200).json({
+        status: "success",
+        data: task
+    });
+})
 
-async function updateTask(req: Request, res: Response) {
+const updateTask = asyncErrorHandler(async (req: Request, res: Response) => {
     const { taskId } = req.params;
-    const updatedTask = req.body;
+    const updatedTask: ITask = req.body;
     // type TaskKey = keyof ITask;
     //TODO Error handling to check if the key exists in ITask 
     // const taskTypes: Record<TaskKey, string> = {}
 
     // for (const [key, value] of Object.entries(updatedTask)) {
     // }
+    const task = await Task.findByIdAndUpdate(taskId, updatedTask, { new: true, runValidators: true, context: "query" });
 
-    try {
-        const task = await Task.findByIdAndUpdate(taskId, updatedTask, { new: true });
+    if (!task) throw new CustomError("Task not found", 404);
 
-        if (!task) res.status(404).send("Task not found");
-        else return res.status(200).json(task);
+    res.status(200).json({
+        status: "success",
+        data: task
+    });
+})
 
-    } catch (e) {
-        console.error("Error details:", e);
-        res.status(500).json("Database query failed!");
-    }
-}
-
-async function createTask(req: Request, res: Response) {
+const createTask = asyncErrorHandler(async (req: Request, res: Response) => {
     const createdTask: ITask = req.body;
 
-    try {
-        const task = await Task.create(createdTask);
+    const task = await Task.create(createdTask);
 
-        return res.status(200).json(task);
+    res.status(200).json({
+        status: "success",
+        data: task
+    });
+})
 
-    } catch (e) {
-        console.error("Error details:", e);
-        res.status(500).json("Database query failed!");
-    }
-}
-
-async function deleteTask(req: Request, res: Response) {
+const deleteTask = asyncErrorHandler(async (req: Request, res: Response) => {
     const { taskId } = req.params;
+    const task = await Task.findByIdAndDelete(taskId);
 
-    try {
-        const task = await Task.findByIdAndDelete(taskId);
+    if (!task) throw new CustomError("Task not found", 404);
 
-        if (!task) res.status(404).send("Task not found");
-        else return res.status(200).json(task);
-
-    } catch (e) {
-        console.error("Error details:", e);
-        res.status(500).json("Database query failed!");
-    }
-}
+    res.status(200).json({
+        status: "success",
+        data: task
+    });
+})
 
 export { getTask, getTasks, updateTask, deleteTask, createTask }
